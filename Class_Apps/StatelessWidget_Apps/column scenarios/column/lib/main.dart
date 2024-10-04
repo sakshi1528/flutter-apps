9d8c8907a7715818b5cd47eade5b0c5b1508bd64
sakshi1528 <sakshibarkade1528>
2024-10-04 15:02:26 +0530

    column

--- Class_Apps/StatelessWidget_Apps/column scenarios/column/lib/main.dart
+++ Class_Apps/StatelessWidget_Apps/column scenarios/column/lib/main.dart
@@ -12,7 +12,30 @@ class MyApp extends StatelessWidget {
     print("Devise width:${MediaQuery.of(context).size.width}");
     print("device height:${MediaQuery.of(context).size.height}");
     return MaterialApp(
-      home:Scaffold
-    )
+      home: Scaffold(
+        appBar:AppBar(
+        title: const Text(
+          "Column Demo",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ),
+    ),);
   }
 }
